aa3f8cea5e1a31bdb9fb59f03caf98595ad66f02
Roberto De Salvo <roberto.desalvo@msc.com>
2024-07-19 16:05:04 +0200

    created data-access project

--- RDS.Fantadepo.Business/Models/Player.cs
+++ RDS.Fantadepo.Business/Models/Player.cs
@@ -8,12 +8,14 @@ namespace RDS.Fantadepo.Business.Models
 {
     public class Player : ICloneable
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
         public object Clone()
         {
             return new Player
             {
+                Id = Id,
                 Name = Name
             };
         }
---
+++ RDS.Fantadepo.DataAccess/Entities/PlayerEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantadepo.DataAccess.Entities
+{
+    public class PlayerEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
