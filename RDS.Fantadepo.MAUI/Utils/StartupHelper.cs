4fb93516deb570057c8256c13515dc72b1bcf4a2
Roberto De Salvo <roberto.desalvo@msc.com>
2024-07-19 16:43:21 +0200

    kv-implementation

---
+++ RDS.Fantadepo.MAUI/Utils/StartupHelper.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantadepo.MAUI.Utils
+{
+    public static class StartupHelper
+    {
+        public static string GetKeyVaultConnectionString()
+        {
+            try
+            {
+                var kvUri = new Uri("https://fantadepo-kv.vault.azure.net/");
+                var credentials = new DefaultAzureCredential();
+                var secretClient = new SecretClient(kvUri, credentials);
+
+                var secret = secretClient.GetSecret("fantadepo-admin-connstring");
+                return secret.Value?.ToString() ?? string.Empty;
+            }
+            catch(Exception ex) 
+            { 
+                return string.Empty;
+            }            
+        }
+    }
+}
