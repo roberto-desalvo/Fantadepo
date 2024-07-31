5d65290c7d96c0c435cc050a5dc9761fd1eb1a40
Roberto De Salvo <roberto.desalvo@msc.com>
2024-07-31 08:14:35 +0200

    added client data access project

---
+++ RDS.Fantedepo.Client/Repositories/TeamRepository.cs
@@ -0,0 +1,53 @@
+﻿using RDS.Fantadepo.Models.Models;
+using RDS.Fantedepo.Client.DataAccess.Repositories.Abstractions;
+using RDS.Fantedepo.Client.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantedepo.Client.DataAccess.Repositories
+{
+    public class TeamRepository : BaseRepository, ITeamRepository
+    {
+        public TeamRepository(Context context) : base(context)
+        {
+        }
+
+        public Task<int> CreateTeam(Team team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTeam(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Team?> GetTeam(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Team>> GetTeams(Func<Team, bool>? predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Team>> GetTeamsWithCoaches(Func<Team, bool>? predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Team?> GetTeamWithCoach(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTeam(int id, Team team)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RDS.Fantedepo.Client/Settings/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantedepo.Client.DataAccess.Settings
+{
+    public class Context
+    {
+        public string Address { get; set; } = string.Empty;
+    }
+}
