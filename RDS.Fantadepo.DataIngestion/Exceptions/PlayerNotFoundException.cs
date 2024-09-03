4c6d9f8d2c72c3a45e70cc4552534a1c2bb92455
roberto-desalvo <roberto.desalvo@outlook.com>
2024-09-03 19:10:35 +0200

    excel reader

---
+++ RDS.Fantadepo.DataIngestion/Exceptions/PlayerNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantadepo.DataIngestion.Exceptions
+{
+    public class PlayerNotFoundException : Exception
+    {
+
+        public PlayerNotFoundException() : base("The player was not found")
+        {
+            
+        }
+        public PlayerNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ RDS.Fantadepo.DataIngestion/Exceptions/TeamAlreadyExistsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDS.Fantadepo.DataIngestion.Exceptions
+{
+    public class TeamAlreadyExistsException : Exception
+    {
+        public TeamAlreadyExistsException() : base("Team cannot be tracked because another team with the same name already exists")
+        {
+            
+        }
+
+        public TeamAlreadyExistsException(string message) : base(message)
+        {
+            
+        }
+    }
+}
